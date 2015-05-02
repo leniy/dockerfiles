d9d5593777ab67243b363224f5d616e96447d31a
Leniy Tsan <m@leniy.org>
2015-05-03 01:07:35 +0800

    set mysql passwd by shell variables

--- setmysqluser.sh
+++ setmysqluser.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#注意，在config文件中，也写入了密码，这儿修改后要同步改过来
+if [ -z $SQL_ROOT_PASSWD ]; then
+		SQL_ROOT_PASSWD="leniy.org";
+fi
+if [ -z $SQL_CACTI_PASSWD ]; then
+		SQL_CACTI_PASSWD="leniy.org";
+fi
+
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
@@ -8,12 +16,10 @@ while [[ RET -ne 0 ]]; do
     mysql -uroot -e "status" > /dev/null 2>&1
     RET=$?
 done
-mysqladmin -uroot password leniy.org
-mysqladmin -uroot -pleniy.org reload
-mysqladmin -uroot -pleniy.org create cacti
-mysql -uroot -pleniy.org cacti < /usr/share/cacti/conf_templates/cacti.sql
-mysql -uroot -pleniy.org -e "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY 'leniy.org'; flush privileges;"
-mysqladmin -uroot -pleniy.org shutdown
 
-
-
+mysqladmin -uroot password $SQL_ROOT_PASSWD
+mysqladmin -uroot -p$SQL_ROOT_PASSWD reload
+mysqladmin -uroot -p$SQL_ROOT_PASSWD create cacti
+mysql -uroot -p$SQL_ROOT_PASSWD cacti < /usr/share/cacti/conf_templates/cacti.sql
+mysql -uroot -p$SQL_ROOT_PASSWD -e "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '$SQL_CACTI_PASSWD'; flush privileges;"
+mysqladmin -uroot -p$SQL_ROOT_PASSWD shutdown
