325d518cdc1d3c693e3eae748180a2d511026e08
Leniy Tsan <m@leniy.org>
2017-01-04 12:50:38 +0800

    restore rra files

--- scripts/cactirestore.sh
+++ scripts/cactirestore.sh
@@ -3,3 +3,6 @@
 
 SQL_CACTI_PASSWD="leniy.org";
 mysql -u cacti -p$SQL_CACTI_PASSWD cacti < /var/cactibackups/latest/cacti_backups.sql
+cp /var/cactibackups/latest/rra_backups.tar.gz /
+cd /
+tar -zxvf rra_backups.tar.gz
